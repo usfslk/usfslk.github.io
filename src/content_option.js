61f33adbbd23f6c0213bf5b6a6faedc3e3da2adb
Youssef Selkani <Ynakles@gmail.com>
2022-07-23 12:21:36 +0100

    configure gh

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "YS";
+const meta = {
+    title: "Youssef Selkani",
+    description: "I’m Youssef Selkani, Data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Youssef Selkani",
+    animated: {
+        first: "Full-stack Developer",
+        second: "Data Scientist",
+        // third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://i.imgur.com/Qlp0Msl.png",
+};
+
+const dataabout = {
+    title: "Professional Objective",
+    aboutme: "Self motivated professional with a proven record of generating and building relationships with SME owners, designing modern user interfaces and managing projects from concept to completion. 7 years of experience remote working independently and within a team. Producing clear, well-documented code. Capability to build and consume all types of APIs. Familiarity with design tools - Illustrator, Sketch, Photoshop."
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "R",
+        value: 50,
+    },
+];
+
+const services = [{
+    title: "C.F. Print",
+    description: "C.F. Print is a mobile app that allows users to print tickets using Bluetooth printers, the app is able to retrieve custom data using web hook triggers from various third-party services (Quickbase)",
+    url: 'https://apps.apple.com/us/app/cfprint/id1629307136'
+},
+{
+    title: "Rthath One",
+    description: "This is a React Native app built from scratch using WooCommerce API and Node.js server to manage authentication, data handling and orders/payment processing.",
+    url: "https://play.google.com/store/apps/details?id=com.rthathone"
+},
+{
+    title: "Netflix",
+    description: "This is a studio tour companion app that can search & load individual studio profiles from surveys database via REST API. Users can verify/annotate/update sections of the profile/add photos, voice notes relative to current profile.",
+    url: "https://play.google.com/store/apps/details?id=com.rthathone"
+
+},
+{
+    title: "CVS Health",
+    description: "This animation contains a countdown timer as well as custom transitions developed entirely with Vanilla JavaScript",
+    url: "https://cvsbanner.firebaseapp.com"
+
+},
+];
+
+const degrees = [{
+    title: "Google Data Analytics Certificate",
+    description: "Google - Jul, 2022",
+    url: 'https://www.credly.com/badges/8a794dbb-1bd9-46e0-8ac6-f7b32d170ddb/public_url'
+},
+{
+    title: "Introduction to APIs in Google Cloud",
+    description: "Google - Mar, 2021",
+    url: "https://coursera.org/verify/UQPAL75F4D6C"
+},
+{
+    title: "Fundamentals of Digital Marketing",
+    description: "Google - Nov, 2018",
+    // url: "https://learndigital.withgoogle.com/digitalgarage/validate-certificate-code"
+},
+];
+
+const dataportfolio = [{
+        img: "https://play-lh.googleusercontent.com/SDBVhXzt-z36epHIJSkMgMQq_1x51aFBG6H_31TF4qtwexv7THQcmjpGSYmmx89YAIY=w2560-h1440",
+        desctiption: "(React Native, Node, Firebase) C.F. Print is a mobile app that allows users to print tickets using Bluetooth printers",
+        link: "https://play.google.com/store/apps/details?id=com.usfslk.cfprint",
+    },
+    {
+        img: "https://i.imgur.com/NN8etbd.png",
+        desctiption: "Ecommerce app built from scratch using WooCommerce API and node.js server to manage authentication, data handling and orders/payment processing",
+        link: "https://play.google.com/store/apps/details?id=com.rthathone",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    degrees
+};
---
+++ src/pages/about/index.js
@@ -0,0 +1,111 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  dataabout,
+  meta,
+  worktimeline,
+  skills,
+  services,
+  degrees,
+} from "../../content_option";
+
+export const About = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> About | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">About me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">{dataabout.title}</h3>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <div>
+              <p>{dataabout.aboutme}</p>
+            </div>
+          </Col>
+        </Row>
+
+        {/* <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Skills</h3>
+          </Col>
+          <Col lg="7">
+            {skills.map((data, i) => {
+              return (
+                <div key={i}>
+                  <h3 className="progress-title">{data.name}</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      style={{
+                        width: `${data.value}%`,
+                      }}
+                    >
+                      <div className="progress-value">{data.value}%</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row> */}
+
+        <Row className="sec_sp">
+          <Col lang="5">
+            <h3 className="color_sec py-4">Employment History</h3>
+          </Col>
+          <Col lg="7">
+            {services.map((data, i) => {
+              return (
+                <div className="service_ py-4" key={i}>
+                  <h5 className="service__title">{data.title}</h5>
+                  <p className="service_desc">{data.description}</p>
+
+                  <div id="button_h" className="ac_btn btn">
+                    <a target="_blank" href={data.url}>
+                      View Project
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+
+        <Row className="sec_sp">
+          <Col lang="5">
+            <h3 className="color_sec py-4">Education</h3>
+          </Col>
+          <Col lg="7">
+            {degrees.map((data, i) => {
+              return (
+                <div className="service_ py-4" key={i}>
+                  <h5 className="service__title">{data.title}</h5>
+                  <p className="service_desc">{data.description}</p>
+
+                  <div id="button_h" className="ac_btn btn">
+                    <a target="_blank" href={data.url}>
+                      Verify
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </HelmetProvider>
+  );
+};
